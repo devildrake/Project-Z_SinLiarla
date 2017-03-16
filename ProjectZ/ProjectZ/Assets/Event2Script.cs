06a862b198bf62957488357dc1c6e4c1a807fd7e
Devildrake <davidsierragonzalez@enti.cat>
2017-03-16 17:38:06 +0100

    Establecido el script de eventos del nivel 2, hay que hacerlo, y de paso COGER EL PUTO RETRASO DEL EVENT1SCRIPT que esta en el nivel 2 y borrarlo

---
+++ ProjectZ/ProjectZ/Assets/Event2Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event2Script : MonoBehaviour {
+    public GameLogicScript gameLogic;
+    public bool[] hasHappened;
+
+    // Use this for initialization
+    void Start () {
+        hasHappened = new bool[3];
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
