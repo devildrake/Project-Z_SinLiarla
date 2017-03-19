6fda4a3095db7b75fe644d830bfd16375775281c
Rattaspi <a.canutperdigon@gmail.com>
2017-03-19 20:37:58 +0100

    Actualizadas algunas cosillas del menu principal.

---
+++ ProjectZ/ProjectZ/Assets/BlinkMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkMenu : MonoBehaviour {
+
+    private Image img;
+    private float timer;
+    private bool active;
+
+	// Use this for initialization
+	void Start () {
+        active = true;
+        timer = 0;
+        img = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer > 0.5f){
+            active = !active;
+            img.enabled = active;
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+        if (Input.anyKey) Destroy(gameObject);
+	}
+}
