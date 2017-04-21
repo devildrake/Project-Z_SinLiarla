fd540bed37f6921f5a927052fda4a1546e784d23
ENTI\cmarruedo <carlosmarruedo@hotmail.com>
2017-04-21 11:42:21 +0200

    Music Manager script añadido con su MusicList y varias musicas pa probar, para mas preguntas vayan a recepcion

---
+++ ProjectZ/ProjectZ/Assets/Scripts/MusicPlaylist.cs
@@ -0,0 +1,34 @@
+﻿/*Este código está pensado para manejar la lógica de selección y movimiento de los zombies, así como el listado de estos 
+ y de los villagers en partida*/
+/*Se hace una referencia general al InputHandler
+*/
+using UnityEngine;
+using System.Collections;
+
+public class MusicPlaylist : MonoBehaviour
+{
+    public bool ActivateOnAwake = true;
+    public AudioClip[] MusicList;
+
+    void Awake()
+    {
+        if (ActivateOnAwake && MusicManager.Instance)
+            MusicManager.Instance.ChangePlaylist(this);
+    }
+
+    void Start()
+    {
+        // Have playlist persist across scenes.
+        DontDestroyOnLoad(gameObject); // Don't destroy this object
+
+        // When a new scene is loaded, destroy the other playlists.
+        foreach (MusicPlaylist playlist in GameObject.FindObjectsOfType<MusicPlaylist>())
+        {
+            if (playlist.name != this.name)
+            {
+                Destroy(playlist.gameObject);
+            }
+        }
+    }
+
+}
