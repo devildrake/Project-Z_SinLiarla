e0d89a647c8444b7d722d32237f3f592aa378522
Rattaspi <a.canutperdigon@gmail.com>
2017-05-25 13:05:47 +0200

    Actualizado el script de parpadeo de las minas y añadido el sprite para el tutorial (el de la vida)

--- ProjectZ/ProjectZ/Assets/Scripts/BlinkLight.cs
+++ ProjectZ/ProjectZ/Assets/Scripts/BlinkLight.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class BlinkLight : MonoBehaviour {
     private Light l;
-    private float time; //tiempo de intermitencias
+    private float time1, time2, startT; //tiempo de intermitencias
     private float count; //tiempo acumulado
+    private bool started; //empieza el comportamiento normal, de base tienen un delay para iniciarse
 
 	// Use this for initialization
 	void Start () {
         l = GetComponent<Light>();
-        time = Random.Range(0.9f,1.0f);
+        time1 = Random.Range(0.4f, 0.6f);
+        startT = Random.Range(0.3f, 0.7f);
+        time2 = Random.Range(2.0f, 2.5f);
+        started = false;
         count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (count > time) {
-            l.enabled = !l.enabled;
-            count = 0;
-            time = Random.Range(0.9f, 1.0f);
+        if (!started) {
+            if (startT < count) {
+                count = 0;
+                started = true;
+            }
+            count += Time.deltaTime;
         }
-        count += Time.deltaTime;
+        else {
+            if (l.enabled) {
+                if (time1 < count) {
+                    count = 0;
+                    l.enabled = false;
+                }
+                count += Time.deltaTime;
+            }
+            else {
+                if (time2 < count) {
+                    count = 0;
+                    l.enabled = true;
+                }
+                count += Time.deltaTime;
+            }
+        }
 	}
 }
